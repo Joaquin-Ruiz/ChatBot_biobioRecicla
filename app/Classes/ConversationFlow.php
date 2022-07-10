b555641378b30ab3571289cafe5e5a85e4750f6f
IQuinterosNB <i.quinteros@hotmail.com>
2022-07-09 23:39:18 -0400

    Working with accents bug

--- app/Classes/ConversationFlow.php
+++ app/Classes/ConversationFlow.php
@@ -216,21 +216,17 @@ class ConversationFlow{
                 }, ARRAY_FILTER_USE_BOTH);
             }
             else {
-                $tok = new WhitespaceTokenizer();
-                $J = new JaccardIndex();
-                $cos = new CosineSimilarity();
-                $simhash = new Simhash(16);
                 
 
                 // Get selected Typed button
-                $foundButtons = array_filter($botResponse->buttons, function(ChatButton $value, $key)  
-                use($thisContext, $answer, $tok, $J, $cos, $simhash, $botResponse){
-
-                    $s1 = ConversationFlow::remove_accents(strtolower($answer->getText()));   
-                    $s2 = ConversationFlow::remove_accents(strtolower($value->text));
+                foreach($botResponse->buttons as $value){
+                    $s1 = mb_strtolower(ConversationFlow::remove_accents($answer->getText()));   
+                    $s2 = mb_strtolower(ConversationFlow::remove_accents($value->text));
 
                     $s1 = preg_replace('/[^A-Za-z0-9 ]/', '', $s1);
                     $s2 = preg_replace('/[^A-Za-z0-9 ]/', '', $s2);
+
+                    Storage::disk('public')->put('testdebug.txt', $s1.' / '.$s2);
 
                     $averageScore = NlpScore::Zero();
 
@@ -247,20 +243,25 @@ class ConversationFlow{
                         $weight = 0.5/$keywordsCount;
 
                         $idealScore = new NlpScore(0.25, 0.35, 0.5);
+                        $perfectScore = new NlpScore(0.95, 0.95, 0.95);
                         if(
+                            $keywordNlpScore->valueA >= $perfectScore->valueA
+                            && $keywordNlpScore->valueB >= $perfectScore->valueB
+                            && $keywordNlpScore->valueC >= $perfectScore->valueC
+                        ) {  $averageScore = new NlpScore(1, 1, 1); }
+                        else if(
                             $keywordNlpScore->valueA >= $idealScore->valueA
                             && $keywordNlpScore->valueB >= $idealScore->valueB
                             && $keywordNlpScore->valueC >= $idealScore->valueC
-                        ) $weight = 1;
+                        ) { $weight = .5; }
+                        
 
                         $keywordNlpScore->scale($weight);
                         $averageScore->add($keywordNlpScore);
                     }
 
                     $botResponse->additionalParams["keywords"] = $value->additionalKeywords;
-                    $botResponse->additionalParams["nlpscore"] = $averageScore;
-
-                    Storage::disk('public')->put('testdebug.txt', '\n'.$averageScore->valueA.'/'.$averageScore->valueB.'/'.$averageScore->valueC);
+                    $botResponse->additionalParams[$value->text] = $averageScore;
 
                     $requiredScore = ($keywordsCount <= 0)? new NlpScore(0.22, 0.5, 0.5) : new NlpScore(0.2, 0.23, 0.3);
 
@@ -269,20 +270,16 @@ class ConversationFlow{
                         && $averageScore->valueB >= $requiredScore->valueB
                         && $averageScore->valueC >= $requiredScore->valueC
                     )){
-                        return true;
+                        $foundButtons[$averageScore->valueA + $averageScore->valueB + $averageScore->valueC] = $value;
                     } else if($averageScore->valueA >= 0.15 && $averageScore->valueB >= 0.3 && $averageScore->valueC >= 0.4){
                         $botResponse->additionalParams['lowProbability'] = true;
                         ConversationFlow::$lowProbability[$averageScore->valueA + $averageScore->valueB + $averageScore->valueC] = clone $value;
                     }
-
-                    $botResponse->additionalParams['resulterror'] = (string)$averageScore->valueA.'/'.(string)$averageScore->valueB.'/'.(string)$averageScore->valueC;
-
-                    return false;
-                }, ARRAY_FILTER_USE_BOTH);
+                }
             }
 
             // Just check if selected button is found
-            if(count($foundButtons) <= 0 || count($foundButtons) > 1){
+            if(count($foundButtons) <= 0){
                 // If not found, display error message and repeat question
                 if($botResponse->errorMessage != null) 
                     $this->say($botResponse->errorMessage, $botResponse->additionalParams);
@@ -313,8 +310,18 @@ class ConversationFlow{
                 );
             }
 
-            // Get first found button 
-            $foundButton = array_shift($foundButtons); 
+            $foundButton = null;
+
+            if(count($foundButtons) > 1){
+                ksort($foundButtons);
+                $foundButton = end($foundButtons);
+            } else {
+                // Get first found button 
+                $foundButton = array_shift($foundButtons); 
+            }
+
+            
+            
 
             // Add selected button to responses array
             array_push($thisContext->responses, $foundButton->text);
@@ -369,108 +376,12 @@ class ConversationFlow{
     }
 
     public static function remove_accents($string) {
-        if ( !preg_match('/[\x80-\xff]/', $string) )
-        return $string;
-
-        $chars = array(
-        // Decompositions for Latin-1 Supplement
-        chr(195).chr(128) => 'A', chr(195).chr(129) => 'A',
-        chr(195).chr(130) => 'A', chr(195).chr(131) => 'A',
-        chr(195).chr(132) => 'A', chr(195).chr(133) => 'A',
-        chr(195).chr(135) => 'C', chr(195).chr(136) => 'E',
-        chr(195).chr(137) => 'E', chr(195).chr(138) => 'E',
-        chr(195).chr(139) => 'E', chr(195).chr(140) => 'I',
-        chr(195).chr(141) => 'I', chr(195).chr(142) => 'I',
-        chr(195).chr(143) => 'I', chr(195).chr(145) => 'N',
-        chr(195).chr(146) => 'O', chr(195).chr(147) => 'O',
-        chr(195).chr(148) => 'O', chr(195).chr(149) => 'O',
-        chr(195).chr(150) => 'O', chr(195).chr(153) => 'U',
-        chr(195).chr(154) => 'U', chr(195).chr(155) => 'U',
-        chr(195).chr(156) => 'U', chr(195).chr(157) => 'Y',
-        chr(195).chr(159) => 's', chr(195).chr(160) => 'a',
-        chr(195).chr(161) => 'a', chr(195).chr(162) => 'a',
-        chr(195).chr(163) => 'a', chr(195).chr(164) => 'a',
-        chr(195).chr(165) => 'a', chr(195).chr(167) => 'c',
-        chr(195).chr(168) => 'e', chr(195).chr(169) => 'e',
-        chr(195).chr(170) => 'e', chr(195).chr(171) => 'e',
-        chr(195).chr(172) => 'i', chr(195).chr(173) => 'i',
-        chr(195).chr(174) => 'i', chr(195).chr(175) => 'i',
-        chr(195).chr(177) => 'n', chr(195).chr(178) => 'o',
-        chr(195).chr(179) => 'o', chr(195).chr(180) => 'o',
-        chr(195).chr(181) => 'o', chr(195).chr(182) => 'o',
-        chr(195).chr(182) => 'o', chr(195).chr(185) => 'u',
-        chr(195).chr(186) => 'u', chr(195).chr(187) => 'u',
-        chr(195).chr(188) => 'u', chr(195).chr(189) => 'y',
-        chr(195).chr(191) => 'y',
-        // Decompositions for Latin Extended-A
-        chr(196).chr(128) => 'A', chr(196).chr(129) => 'a',
-        chr(196).chr(130) => 'A', chr(196).chr(131) => 'a',
-        chr(196).chr(132) => 'A', chr(196).chr(133) => 'a',
-        chr(196).chr(134) => 'C', chr(196).chr(135) => 'c',
-        chr(196).chr(136) => 'C', chr(196).chr(137) => 'c',
-        chr(196).chr(138) => 'C', chr(196).chr(139) => 'c',
-        chr(196).chr(140) => 'C', chr(196).chr(141) => 'c',
-        chr(196).chr(142) => 'D', chr(196).chr(143) => 'd',
-        chr(196).chr(144) => 'D', chr(196).chr(145) => 'd',
-        chr(196).chr(146) => 'E', chr(196).chr(147) => 'e',
-        chr(196).chr(148) => 'E', chr(196).chr(149) => 'e',
-        chr(196).chr(150) => 'E', chr(196).chr(151) => 'e',
-        chr(196).chr(152) => 'E', chr(196).chr(153) => 'e',
-        chr(196).chr(154) => 'E', chr(196).chr(155) => 'e',
-        chr(196).chr(156) => 'G', chr(196).chr(157) => 'g',
-        chr(196).chr(158) => 'G', chr(196).chr(159) => 'g',
-        chr(196).chr(160) => 'G', chr(196).chr(161) => 'g',
-        chr(196).chr(162) => 'G', chr(196).chr(163) => 'g',
-        chr(196).chr(164) => 'H', chr(196).chr(165) => 'h',
-        chr(196).chr(166) => 'H', chr(196).chr(167) => 'h',
-        chr(196).chr(168) => 'I', chr(196).chr(169) => 'i',
-        chr(196).chr(170) => 'I', chr(196).chr(171) => 'i',
-        chr(196).chr(172) => 'I', chr(196).chr(173) => 'i',
-        chr(196).chr(174) => 'I', chr(196).chr(175) => 'i',
-        chr(196).chr(176) => 'I', chr(196).chr(177) => 'i',
-        chr(196).chr(178) => 'IJ',chr(196).chr(179) => 'ij',
-        chr(196).chr(180) => 'J', chr(196).chr(181) => 'j',
-        chr(196).chr(182) => 'K', chr(196).chr(183) => 'k',
-        chr(196).chr(184) => 'k', chr(196).chr(185) => 'L',
-        chr(196).chr(186) => 'l', chr(196).chr(187) => 'L',
-        chr(196).chr(188) => 'l', chr(196).chr(189) => 'L',
-        chr(196).chr(190) => 'l', chr(196).chr(191) => 'L',
-        chr(197).chr(128) => 'l', chr(197).chr(129) => 'L',
-        chr(197).chr(130) => 'l', chr(197).chr(131) => 'N',
-        chr(197).chr(132) => 'n', chr(197).chr(133) => 'N',
-        chr(197).chr(134) => 'n', chr(197).chr(135) => 'N',
-        chr(197).chr(136) => 'n', chr(197).chr(137) => 'N',
-        chr(197).chr(138) => 'n', chr(197).chr(139) => 'N',
-        chr(197).chr(140) => 'O', chr(197).chr(141) => 'o',
-        chr(197).chr(142) => 'O', chr(197).chr(143) => 'o',
-        chr(197).chr(144) => 'O', chr(197).chr(145) => 'o',
-        chr(197).chr(146) => 'OE',chr(197).chr(147) => 'oe',
-        chr(197).chr(148) => 'R',chr(197).chr(149) => 'r',
-        chr(197).chr(150) => 'R',chr(197).chr(151) => 'r',
-        chr(197).chr(152) => 'R',chr(197).chr(153) => 'r',
-        chr(197).chr(154) => 'S',chr(197).chr(155) => 's',
-        chr(197).chr(156) => 'S',chr(197).chr(157) => 's',
-        chr(197).chr(158) => 'S',chr(197).chr(159) => 's',
-        chr(197).chr(160) => 'S', chr(197).chr(161) => 's',
-        chr(197).chr(162) => 'T', chr(197).chr(163) => 't',
-        chr(197).chr(164) => 'T', chr(197).chr(165) => 't',
-        chr(197).chr(166) => 'T', chr(197).chr(167) => 't',
-        chr(197).chr(168) => 'U', chr(197).chr(169) => 'u',
-        chr(197).chr(170) => 'U', chr(197).chr(171) => 'u',
-        chr(197).chr(172) => 'U', chr(197).chr(173) => 'u',
-        chr(197).chr(174) => 'U', chr(197).chr(175) => 'u',
-        chr(197).chr(176) => 'U', chr(197).chr(177) => 'u',
-        chr(197).chr(178) => 'U', chr(197).chr(179) => 'u',
-        chr(197).chr(180) => 'W', chr(197).chr(181) => 'w',
-        chr(197).chr(182) => 'Y', chr(197).chr(183) => 'y',
-        chr(197).chr(184) => 'Y', chr(197).chr(185) => 'Z',
-        chr(197).chr(186) => 'z', chr(197).chr(187) => 'Z',
-        chr(197).chr(188) => 'z', chr(197).chr(189) => 'Z',
-        chr(197).chr(190) => 'z', chr(197).chr(191) => 's'
-        );
-
-        $string = strtr($string, $chars);
-
+        $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+        $string = strtr( $string, $unwanted_array );
         return $string;
     }
 }
