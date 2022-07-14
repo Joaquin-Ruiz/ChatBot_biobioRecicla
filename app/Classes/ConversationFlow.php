a75e15b7001a0948640fc1f7c0e1ef835e779763
IQuinterosNB <i.quinteros@hotmail.com>
2022-07-13 22:48:17 -0400

    Remove comments

--- app/Classes/ConversationFlow.php
+++ app/Classes/ConversationFlow.php
@@ -99,11 +99,7 @@ class ConversationFlow{
 
     
     public static $lowProbability;
-    public function create_question($context, BotResponse $botResponse, ?BotResponse $rootResponse = null){   
-        // TODO: Detect multiple responses from Questions with process
-        // Example: Hola, quiero de color rojo, también me gustaría verde y finalmente azul
-        // Resultado: Ok, aqui tiene rojo, verde y azul
-        
+    public function create_question($context, BotResponse $botResponse, ?BotResponse $rootResponse = null){           
         
         // Context is required
         if($context == null) return;
